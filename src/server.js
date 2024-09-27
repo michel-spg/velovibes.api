eb67100b9e76847e88b9b71ddcad85429b35184d
mip <michel@spengergasse.at>
2024-09-27 13:16:58 +0200

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bikes = require("./bikes");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Welcome to VeloVibes!");
+});
+
+app.get("/bikes", (req, res) => {
+  res.json(bikes);
+});
+
+app.get("/bikes/:bikeId", (req, res) => {
+    const bikeId = req.params.bikeId;
+    const bike = bikes.find(bike => bike.id == bikeId);
+    if (!bike) {
+        res.status(404).send("Bike not found");
+    } else {
+        res.json(bike);
+    }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
